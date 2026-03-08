1b9491ab130bd5389fab8374aa7405c24821ebe9
Pinchas Lifshitz <pnl8480537@gmail.com>
2026-03-08 22:58:03 +0200

    added a middleware in login function

--- server/middleware/loginError.js
+++ server/middleware/loginError.js
@@ -1,4 +1,4 @@
-export function errorReq(req, res, next) {
+export function loginError(req, res, next) {
     const { agentCode, password } = req.body
     if (!agentCode || !password) return res.status(400).json({ message: "One or more of the inputs were not entered correctly!" })
     else if (typeof agentCode !== "string" || typeof password !== "string") return res.status(401).json({ message: "Invalid input was entered." })
--- server/routes/authR.js
+++ server/routes/authR.js
@@ -1,8 +1,9 @@
 import {Router} from "express"
 import { login } from "../controller/authC.js"
+import { loginError } from "../middleware/loginError.js"
 
 const router = Router()
 
-router.post("/login",login)
+router.post("/login",loginError,login)
 
 export default router
